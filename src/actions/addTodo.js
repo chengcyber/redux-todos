89806acc30dadb28a3e50fb8cb8b88555f84f979
kimochg <kimochg@outlook.com>
2017-01-23 15:22:10 +0800

    feat(store): saveState wrapped in throttle function from lodash
    Since JSON.stringify is so expensive, we need prevent saveState more often than 1 second wiht
    
    throttle from lodash module

--- src/actions/addTodo.js
+++ src/actions/addTodo.js
@@ -1,5 +1,8 @@
 import { v4 } from 'node-uuid';
 
+/**
+ * Using node.uuid v4 method to generate unique id
+ */
 const addTodo = (text) => {
     return {
         type: 'ADD_TODO',
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers, createStore }from 'redux';
 import todos from './todos.js';
 import visibilityFilter from './visibilityFilter.js';
 import { loadState, saveState } from '../modules/localStorage'
+import throttle from 'lodash/throttle';
 
 const todoApp = combineReducers({
     todos,
@@ -14,11 +15,14 @@ const todoApp = combineReducers({
  */
 const presistedState = loadState()
 const store = createStore(todoApp, presistedState)
-store.subscribe(() => {
+/**
+ * Throttle here to prevent expensive JSON.stringify in saveState() more often than 1 sec
+ */
+store.subscribe(throttle(() => {
     const todos = store.getState().todos
     saveState({
         todos
     })
-})
+}), 1000)
 
 export default store;
