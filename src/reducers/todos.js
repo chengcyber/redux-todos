a6b49b82969a9e3e915b9d8d3a5a383bfcc4dcba
kimochg <kimochg@outlook.com>
2017-01-24 08:58:20 +0800

    refactor(reducers): refactor todos as a array of index id to perform more like database
    byid as array-like object, each todo can be access by action id, allIds as a array of index id

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,24 +1,56 @@
-import todo from './todo.js'
+import todo from './todo.js';
+import { combineReducers } from 'redux';
+//
+// const todos = (state = [], action) => {
+//     switch (action.type) {
+//         case 'ADD_TODO':
+//             return [
+//                 ...state,
+//                 todo(undefined, action)
+//             ]
+//         case 'TOGGLE_TODO': {
+//             return state.map(t =>
+//                 todo(t, action)
+//             )
+//         }
+//         default: return state
+//     }
+// }
 
-const todos = (state = [], action) => {
+const byId = (state = {}, action) => {
     switch (action.type) {
         case 'ADD_TODO':
-            return [
+        case 'TOGGLE_TODO':
+            return {
                 ...state,
-                todo(undefined, action)
-            ]
-        case 'TOGGLE_TODO': {
-            return state.map(t =>
-                todo(t, action)
-            )
-        }
-        default: return state
+                [action.id]: todo(state[action.id], action)
+            }
+        default:
+            return state;
     }
 }
 
+const allIds = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, action.id]
+        default:
+            return state;
+    }
+}
+
+const todos = combineReducers({
+    byId,
+    allIds
+})
+
 export default todos;
 
-export const getVisibleTodos = (todos, filter) => {
+const getAllTodos = (state) =>
+    state.allIds.map( id => state.byId[id]);
+
+export const getVisibleTodos = (state, filter) => {
+    const todos = getAllTodos(state)
     switch (filter) {
         case 'all': return todos;
         case 'active':　return todos.filter(t => !t.completed)
